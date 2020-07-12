2a7849a454169c5d06ea2662a20a14793a181fd6
Kevin Volanski <kvolanski@gmail.com>
2020-07-12 00:17:40 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,14 @@
+import { StorageService } from './../services/storage.service';
 import { Observable } from 'rxjs/Rx';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
+
+    constructor(public storage : StorageService){
+
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         return next.handle(req)
@@ -20,9 +25,20 @@ export class ErrorInterceptor implements HttpInterceptor{
             console.log("Erro detectado pelo interceptor:");
             console.log(errorObj);
 
+            switch(errorObj.status){
+                case 403:
+                this.handle403();
+                break;
+            }
+            
             return Observable.throw(errorObj);
         }) as any;
     }
+
+    handle403(){
+        this.storage.setLocalUser(null);
+    }
+    
 }
 
 export const ErrorInterceptorProvider = {
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -2,7 +2,7 @@ import { StorageService } from './../storage.service';
 import { API_CONFIG } from './../../config/api.config';
 import { ClienteDTO } from './../../models/cliente.dto';
 import { Observable } from 'rxjs/Rx';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 
 @Injectable()
