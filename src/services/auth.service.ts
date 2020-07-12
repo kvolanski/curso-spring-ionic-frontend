39dd9eefe6638e56aac7b15ed25689336ff739d7
Kevin Volanski <kvolanski@gmail.com>
2020-07-12 01:05:45 -0300

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -24,6 +24,14 @@ export class AuthService{
         });
     }
 
+    refreshToken(){
+        return this.http.post(`${API_CONFIG.baseUrl}/auth/refresh_token`,{},
+        {
+            observe:'response',
+            responseType: 'text'
+        });
+    }
+
     succesfulLogin(authorizationValue : string){
         let tok = authorizationValue.substring(7);
         let user: LocalUser = {
