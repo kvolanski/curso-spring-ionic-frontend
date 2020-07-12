2ce7191f35535c7007d821c0c9606581a912b278
Kevin Volanski <kvolanski@gmail.com>
2020-07-12 10:01:42 -0300

    Povoando dinamicamente estado e cidade

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,3 +1,7 @@
+import { CidadeDTO } from './../../models/cidade.dto';
+import { EstadoDTO } from './../../models/estado.dto';
+import { EstadoService } from './../../services/domain/estado.service';
+import { CidadeService } from './../../services/domain/cidade.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -10,8 +14,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignupPage {
 
   formGroup: FormGroup;
+  estados: EstadoDTO[];
+  cidades: CidadeDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public cidadeService: CidadeService, public estadoService: EstadoService) {
 
     this.formGroup = this.formBuilder.group({
       nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
@@ -31,6 +37,24 @@ export class SignupPage {
       cidadeId : [null, [Validators.required]]      
     });
 
+  }
+
+  ionViewDidLoad(){
+   this.estadoService.findAll().subscribe(response => {
+     this.estados = response;
+     this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+     this.updateCidades();
+   },
+   error => {})
+  }
+
+  updateCidades(){
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id).subscribe(response => {
+      this.cidades = response;
+      this.formGroup.controls.cidadeId.setValue(null);
+    },
+    error => {})
   }
 
   signupUser(){
---
+++ src/services/domain/estado.service.ts
@@ -0,0 +1,18 @@
+import { EstadoDTO } from './../../models/estado.dto';
+import { API_CONFIG } from './../../config/api.config';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class EstadoService{
+
+    constructor(public http: HttpClient){
+
+    }
+
+    findAll() : Observable<EstadoDTO[]>{
+        return this.http.get<EstadoDTO[]>(`${API_CONFIG.baseUrl}/estados`);
+    }
+
+}
